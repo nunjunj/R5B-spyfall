1726a07d78fc5ae18632fbf662ded78c9a013a27
nunjunj <junlajak@berkeley.edu>
2023-04-14 23:42:35 -0700

    Add brief instruction

--- pages/how-to-play.js
+++ pages/how-to-play.js
@@ -7,30 +7,49 @@ const HowToPlay = () => {
 		<>
 			{/* <Layout>  */}
 			<div className="main-menu">
-				<h3>How to Play Spyfall</h3>
+				<h3>How to Play Mafia Report</h3>
 				<hr />
 				<div>
-					<p>
+					{/* <p>
 						Spyfall is based off of the board game of the same name, so if
 						you've played that before, you'll understand the web version. You'll
 						need at least 3 or 4 players, but Spyfall is best with 5 or 6
 						players. You can also have 7 or more players, but the game may
 						become too easy for the spy.
-					</p>
+					</p> */}
 					<p>
+						Mafia Report is a board game for 4-5 people. Each person is required
+						to have their own device (phone/laptop). To start the game, one
+						player will create a room and share the room code to other players.
+						At the beginning of the game, all players will receive a secret
+						word, except one spy. The spy and all players will receive the same
+						choices of words Players will then takes turn asking questions to
+						each other to determine who the spy is. The spy can win in two ways.
+						One is that the time is out before other players can identify the
+						spy or they vote the wrong spy. The other way is in the case that
+						the spy know the secret word, they can say the word and wins the
+						game immediately. Other players should not make it too obvious what
+						the secret word is, while asking questions.
+					</p>
+
+					<p>
+						To test the game, please create a room in one device and join the
+						room in another device
+					</p>
+					{/* <p>
 						In my opinion, the easiest way to learn how to play Spyfall would be
 						to watch someone else play, as it will help you get a feel for the
 						dynamic of the game. Here's a video on YouTube of some people
 						playing:
-					</p>
-					<div style={{ textAlign: "center" }}>
+					</p> */}
+					{/* <div style={{ textAlign: "center" }}>
 						<iframe
 							src="https://www.youtube.com/embed/O7W0rH6YpeI"
 							className="embed-responsive-item"
 						/>
-					</div>
+					</div> */}
 					<br />
-					<p>
+					{/* <p>
 						In short, when the game begins, one random player will become the
 						spy, and all others will be given a location, and a role within the
 						location. For example, if the location of a round was "restaurant,"
@@ -45,8 +64,8 @@ const HowToPlay = () => {
 						during the round. The spy also wins if the other players unanimously
 						accuse someone else, or cannot unanimously decide on someone to
 						accuse.
-					</p>
-					<p>
+					</p> */}
+					{/* <p>
 						If you want a little more detail, you can read the full official
 						rulebook of the board game version of Spyfall{" "}
 						<a
@@ -57,7 +76,7 @@ const HowToPlay = () => {
 							here
 						</a>
 						.
-					</p>
+					</p> */}
 
 					<style jsx>{`
 						text-align: left;
@@ -66,7 +85,7 @@ const HowToPlay = () => {
 			</div>
 			{/* </Layout> */}
 			<Link href="/">
-				<button>Back to Spyfall</button>
+				<button>Back to HomePage</button>
 			</Link>
 		</>
 	);
